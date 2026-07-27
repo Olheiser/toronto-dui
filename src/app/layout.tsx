d0d05af0f74b73ffbd96641e7f399f8485cb529c
Olheiser <tanner.olheiser@gmail.com>
2026-07-27 12:51:48 -0600

    Updated tagging

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,23 @@ export default function RootLayout({
   return (
     <html lang="en">
        <head>
+      {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-P8H6T97L');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
+
   {/* Google tag (gtag.js) */}
   <Script
     src="https://www.googletagmanager.com/gtag/js?id=G-DNECBV6P2W"
@@ -45,6 +62,14 @@ export default function RootLayout({
     }}
   />
 </head>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P8H6T97L"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
       <body>{children}</body>
     </html>
   );
