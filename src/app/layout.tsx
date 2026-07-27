ac095470daa46e6daf2426c50f5b1cb4daf126be
Olheiser <tanner.olheiser@gmail.com>
2026-07-27 10:45:48 -0600

    Updated analytics tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
        <head>
   {/* Google tag (gtag.js) */}
   <Script
-    src="https://www.googletagmanager.com/gtag/js?id=AW-653876065"
+    src="https://www.googletagmanager.com/gtag/js?id=G-DNECBV6P2W"
     strategy="afterInteractive" // Ensures script runs after page load
   />
   <Script
@@ -40,7 +40,7 @@ export default function RootLayout({
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
 
-        gtag('config', 'AW-653876065');
+        gtag('config', 'G-DNECBV6P2W');
       `,
     }}
   />
